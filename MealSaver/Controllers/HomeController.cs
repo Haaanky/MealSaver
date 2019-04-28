13b6a5a7519c53870e43f1c4aa80c53d1b774767
Henrik NF <Henriknorfri@gmail.com>
2019-04-28 15:42:40 +0200

    inital commit

--- MealSaver/Controllers/HomeController.cs
+++ MealSaver/Controllers/HomeController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MealSaver.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MealSaver.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserService userService;
+
+        public HomeController(UserService userService)
+        {
+            this.userService = userService;
+        }
+        [Route("")]
         public IActionResult Index()
         {
             return View();
