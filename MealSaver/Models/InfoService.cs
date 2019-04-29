19f19d3d591431208b6ae7ba24c93abfe667a098
Elin Strandberg <elin-strandberg@hotmail.com>
2019-04-29 13:28:22 +0200

    Worked on About View

---
+++ MealSaver/Models/InfoService.cs
@@ -0,0 +1,25 @@
+﻿using MealSaver.Models.ViewModels.Info;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealSaver.Models
+{
+    public class InfoService
+    {
+        List<InfoAboutVM> founders = new List<InfoAboutVM>
+        {
+            new InfoAboutVM{ Id = 1, Image = "/HenrikCheng.jpg", Name = "Henrik Cheng", Description = "Henrik har en bakgrund från sjukvården med läkarexamen från Karolinska Institutet och yrkeserfarenhet efter AT-tjänstgöring. Läkaryrket har format Henrik till en person som är effektiv i sitt arbete och givit honom vana att arbeta i team. Henrik har ett stort intresse för IT, problemlösning och programmering. Han är alltid intresserad av att lära sig nya saker och utvecklas. Genom Academy vill Henrik utvecklas i en ny riktning som utvecklare och bidra med sin problemlösningsförmåga, kreativitet samt positiva energi." },
+            new InfoAboutVM{ Id = 2, Image = "/elin.jpg", Name = "Elin Strandberg", Description = "Elin är en fysioterapeut med en masterexamen i folkhälsovetenskap. Tidigare arbete med verksamhetskoordinering, statistikbearbetning och forskning gör att hon tar sig an nya utmaningar på ett analytiskt, strukturerat och kreativt sätt. Tack vare erfarenheter av kundsupport, sjukvård och kommunal service har hon även utvecklat stor social kompetens samt förståelse för människor. Intresset för programmering väcktes när Elin arbetade med webbtillgänglighet. Nu ser hon framemot en karriär inom IT-branschen där hon vill bidra till att utveckla användarvänliga digitala lösningar från grunden." },
+            new InfoAboutVM{ Id = 3, Image = "/HenrikNF.jpg", Name = "Henrik Norlin Frisemo", Description = "Henrik har en kandidatexamen inom medicinsk biologi och har studerat kurser på masternivå vid Linköpings universitet. Han trivdes med att samarbeta inom olika projektformer då kurserna ofta drevs i projekt med fokus på problemlösning. Henrik arbetade tidigare som manager på en pub, ett yrke som han tyckte mycket om eftersom han fick arbeta med service och kundbemötande. I denna yrkesroll fick han utmana sig själv och hitta effektiva lösningar. Han är social, nyfiken och har ett stort IT- samt teknikintresse. Genom Academy får han nu möjlighet att förvandla sin hobby till en karriär." },
+        };
+
+        public InfoAboutVM[] GetallFounders()
+        {
+            return founders
+                .Select(o => new InfoAboutVM { Id = o.Id, Name = o.Name, Description= o.Description, Image = o.Image })
+                .ToArray();
+        }
+    }
+}
--- MealSaver/Models/ViewModels/Info/InfoContactVM.cs
+++ MealSaver/Models/ViewModels/Info/InfoContactVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MealSaver.Models.ViewModels
+namespace MealSaver.Models.ViewModels.Info
 {
     public class InfoContactVM
     {
