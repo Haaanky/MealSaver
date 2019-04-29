fd1a26497da1f3b891207633a04472067baedc2c
Henrik NF <Henriknorfri@gmail.com>
2019-04-29 10:45:50 +0200

    added lots of stuff

---
+++ MealSaver/Models/ViewModels/User/UserAddItemVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealSaver.Models.ViewModels.User
+{
+    public enum UnitMeasurement
+    {
+        Kg,
+        g,
+        dL,
+        L
+    }
+    public class UserAddItemVM
+    {
+        public string ItemName { get; set; }
+        public double ItemAmount { get; set; }
+        public UnitMeasurement Unit { get; set; }
+        public DateTime DateOfInput { get; set; }
+    }
+}
