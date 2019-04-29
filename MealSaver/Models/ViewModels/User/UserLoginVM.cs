6e7c8bd63e0f8b52b3920c439f2b6729f2161a06
Henrik NF <Henriknorfri@gmail.com>
2019-04-29 15:07:22 +0200

    added logic to controllers and UserService

--- MealSaver/Models/ViewModels/User/UserLoginVM.cs
+++ MealSaver/Models/ViewModels/User/UserLoginVM.cs
@@ -9,5 +9,6 @@ namespace MealSaver.Models.ViewModels.User
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
