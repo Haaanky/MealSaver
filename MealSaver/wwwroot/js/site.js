dc121ca330b9a7a5032492ee623d32a8dc342a2e
HenrikCheng <henrikcheng@live.se>
2019-05-02 15:14:13 +0200

    -before merge with dev

--- MealSaver/wwwroot/js/site.js
+++ MealSaver/wwwroot/js/site.js
@@ -1,6 +1,1 @@
-﻿$(function () {
-    $('.scroll-down').click(function () {
-        $('html, body').animate({ scrollTop: $('section.ok').offset().top }, 'slow');
-        return false;
-    });
-});
+﻿
